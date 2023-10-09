14c9e009d4a37a7528bc416378e4882f1072ce67
adityahpatel <adityahpatel@gmail.com>
2023-10-08 19:10:42 -0700

    change 1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<center><h1> Journey of a 1000 miles begins with a single step </h1> <center>"
+    return "<center><h1> Aditya - Journey of a 1000 miles begins with a single step </h1> <center>"
 
 if __name__ == '__main__':
     app.run(debug=True)
